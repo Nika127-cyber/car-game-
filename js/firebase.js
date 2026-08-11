a98d915456c00a8c9f4a8398496bce66bdc0ce22
Nika127-cyber <zuraniauri2@icloud.com>
2026-08-11 16:51:37 +0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBZ2rZilsRo1FuSeOaKEduhWefVQUDWIq4",
+    authDomain: "street-legends-36204.firebaseapp.com",
+    projectId: "street-legends-36204",
+    storageBucket: "street-legends-36204.firebasestorage.app",
+    messagingSenderId: "755646586968",
+    appId: "1:755646586968:web:c95084b38dbc1f7cdd6a2d"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
